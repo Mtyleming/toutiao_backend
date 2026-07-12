777b2581dd3649906f24bc5fd6719abb6e31bbe6
Akira <xiaom6131@gmail.com>
2026-07-12 19:39:37 +0800

    day09 fastApi第九天学习

--- crud/news.py
+++ crud/news.py
@@ -1,8 +1,61 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models.news import Category
-from sqlalchemy import select
+from models.news import Category, News
+from sqlalchemy import select, func
+
 
 async def get_categories(db: AsyncSession,page:int = 0, limit: int = 100):
     result = await db.execute(select(Category).offset(page).limit(limit))
     return result.scalars().all()
+
+async def get_news_list(db:AsyncSession,page:int = 1,page_size:int = 10,category_id:int = None):
+    offset = (page - 1) * page_size
+    query = select(News)
+
+    # 动态添加条件（类似 Java 的 if 判断拼 SQL）
+    if category_id is not None:
+        query = query.where(News.category_id == category_id)
+    # 分页
+    query = query.offset(offset).limit(page_size)
+
+    res = await db.execute(query)
+    return res.scalars().all()
+
+async def get_news_count(db:AsyncSession,category_id:int = None):
+    query = select(func.count(News.id))
+    if category_id is not None:
+        query = query.where(News.category_id == category_id)
+    res = await db.execute(query)
+    return res.scalar_one()
+
+async def get_news_detail(db:AsyncSession,new_id:int):
+    query = select(News).where(News.id == new_id)
+    res = await db.execute(query)
+    return res.scalars().one_or_none()
+
+async def add_news_view(news:News, db:AsyncSession):
+    news.views += 1
+    await db.commit()
+
+async def get_related_news(db:AsyncSession,news_id:int,category_id:int,limit:int):
+    query = select(News).where(
+        News.category_id == category_id,
+        News.id != news_id
+    ).order_by(
+        News.views.desc(),
+        News.publish_time.desc()
+    ).limit(limit)
+    res = await db.execute(query)
+    # return res.scalars().all()
+    resList = res.scalars().all()
+    return [{
+        "id": new.id,
+        "title": new.title,
+        "description": new.description,
+        "content": new.content,
+        "image": new.image,
+        "author": new.author,
+        "publish_time": new.publish_time,
+        "category_id": new.category_id,
+        "views": new.views
+    } for new in resList]
--- routers/news.py
+++ routers/news.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi.params import Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from config.db_confing import get_db
-from crud.news import get_categories
+from crud.news import get_categories, get_news_list, get_news_count, get_news_detail, add_news_view, get_related_news
 
 router = APIRouter(prefix="/api/news", tags=["news"])
 
@@ -19,11 +20,61 @@ async def categories(page:int = 0, limit:int = 100, db: AsyncSession = Depends(g
     }
 
 @router.get("/list")
-async def get_news(page:int = 0, limit:int = 100,category_id:int = None, db: AsyncSession = Depends(get_db)):
+async def get_list(
+        page:int = 1,
+        page_size:int = Query(10, ge=1, le=100,alias="pageSize"),
+        category_id:int = Query(None, ge=0, alias="categoryId"),
+        db: AsyncSession = Depends(get_db)
+):
 
+    news = await get_news_list(db, page, page_size, category_id)
+    num = await get_news_count(db, category_id)
     return {
         "code":200,
         "message": "获取新闻成功",
-        "data": []
+        "total": num,
+        "data": news,
+        "has_more": num > (page-1) * page_size + len(news)  # 是否有更多
     }
 
+@router.get("/count")
+async def get_count(
+        category_id:int = Query(None, ge=0, alias="categoryId"),
+        db: AsyncSession = Depends(get_db)
+):
+
+    num = await get_news_count(db, category_id)
+    return {
+        "code":200,
+        "message": "获取新闻数量成功",
+        "data": num
+    }
+
+@router.get("/getDetail")
+async def get_detail(
+        new_id:int = Query(..., ge=0, alias="newId"),
+        db: AsyncSession = Depends(get_db)
+):
+
+    res = await get_news_detail(db, new_id)
+    if res is None:
+        raise Exception("新闻不存在")
+    await add_news_view(res,db)
+    related_news = await get_related_news(db,res.id,res.category_id,5)
+    return {
+        "code":200,
+        "message": "获取新闻详情成功",
+        "data": {
+            "id": res.id,
+            "title": res.title,
+            "description": res.description,
+            "content": res.content,
+            "image": res.image,
+            "author": res.author,
+            "publish_time": res.publish_time,
+            "category_id": res.category_id,
+            "views": res.views,
+            "relatedNews":related_news
+        }
+
+    }
